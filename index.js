c27457a9ad7244bdb57ef11c19fb62978473519c
akash85246 <akash22164033@akgec.ac.in>
2025-02-18 02:46:12 +0530

    Fix COOP error by setting Cross-Origin-Opener-Policy headers

--- index.js
+++ index.js
@@ -61,6 +61,13 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use((req, res, next) => {
+  res.setHeader("Cross-Origin-Opener-Policy", "same-origin-allow-popups");
+  res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+  res.setHeader("Cross-Origin-Resource-Policy", "cross-origin");
+  next();
+});
+
 app.use("/api/auth", authRoutes);
 app.use("/api/setting", settingRoutes);
 app.use("/api/note", noteRoutes);
